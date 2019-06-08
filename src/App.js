49685a92cc3d029b99c14571cc76662ec058a1bd
Déric Dallaire <dallaire.deric@gmail.com>
2019-06-08 19:05:06 +0200

    Added possibility to remove a todo item

--- src/App.js
+++ src/App.js
@@ -20,12 +20,21 @@ class Todo extends React.Component {
     }
   }
 
+  removeTodo = (e) => {
+    const targetId = Number(e.target.dataset.id);
+    const targetIndex = this.state.todos.findIndex(x => x.id === targetId);
+    this.setState((state, props) => ({
+      todos: [...state.todos.slice(0, targetIndex), ...state.todos.slice(targetIndex + 1)]
+    }));
+  }
+
   render() {
     return <ul>  
       {
         this.state.todos.map(t => 
           <li key={t.id}>
             {t.description}
+            <button data-id={t.id} onClick={this.removeTodo}>X</button>
           </li>
         )
       }  
