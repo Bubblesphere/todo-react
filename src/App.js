a45d8d6dd634fc8993a709478cd59e931f52d007
Déric Dallaire <dallaire.deric@gmail.com>
2019-06-08 19:05:34 +0200

    Added possibility to add a todo item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import uuid from 'uuid/v4';
 
 class Todo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      current: "test",
       todos: [
         {
           id: 1,
@@ -28,8 +30,30 @@ class Todo extends React.Component {
     }));
   }
 
+  addTodo = (todo) => {
+    todo = {id:3, description:"test", dateAdded: new Date()};
+    this.setState((state, props) => ({
+      current: "",
+      todos: [...state.todos, { 
+        id: uuid(), 
+        description: state.current, 
+        done: false
+      }]
+    }));
+  }
+
+  handleCurrent = (e) => {
+    const value = e.target.value;
+    this.setState((state, props) => ({
+      current: value
+    }));
+  }
+
   render() {
-    return <ul>  
+    return <div>
+      <input type="text" value={this.state.current} onChange={this.handleCurrent}/>
+      <button onClick={this.addTodo}>Add</button>
+      <ul>  
       {
         this.state.todos.map(t => 
           <li key={t.id}>
@@ -38,7 +62,8 @@ class Todo extends React.Component {
           </li>
         )
       }  
-    </ul>
+      </ul>
+    </div>
   }
 }
 
